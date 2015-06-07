0917dd870a1003f2f499ce6168b96fc84a0d81a5
Jake Reuben Zidow <jrzidow@ucsc.edu>
2015-06-06 19:50:41 -0700

    Began Filter:CheckCondition

--- qe/qe.cc
+++ qe/qe.cc
@@ -28,11 +28,132 @@ void Filter::getAttributes(vector<Attribute> &attrs)const{
 
 bool Filter::checkCondition(vector<Attribute> descriptor, void* data, Condition condition){
 
+	Attribute leftAttribute;
+	Attribute rightAttribute;
+	unsigned lhsOffset = 0;
+	unsigned rhsOffset = 0;
+	bool found = false;
+`	unsigned i;	
+
+	for( i = 0; i < descriptor.size(); ++i){
+		if(condition.lhsAttr == descriptor[i].name){
+			leftAttribute = descriptor[i];
+			found = true;
+			break;
+		}
+	}
+
+	if (!found){
+		fprintf(stderr, "Filter.checkCondition(): incompatible lhs descriptor\n");
+		return false;
+	}
+
+	if(condition.bRhsIsAttr){
+
+		found = false;
+		for(i = 0; i < descriptor.size(); ++i){
+			if (descriptor[i].name == condition.rhsAttr){		
+				rightAttribute = descriptor[i];
+				found = true;
+				break;
+			}
+		}
+		if(!found){
+			fprintf(stderr, "Filter.checkCondition(): incompatible rhs descriptor\n");
+			return false;
+		}
+
+		if(rightAttribute.type != leftAttribute.type){
+			fprintf(stderr, "Filter.checkCondition() rhs/lhs attribute types don't match\n");
+			return false;
+		}
+
+		if ()
+
+
+	} else{
 
 
 
+	}
 
 }
+
+bool Filter::checkCondition(int dataInt, CompOp compOp, const void * value)
+{
+	// Checking a condition on an integer is the same as checking it on a float with the same value.
+	int valueInt;
+	memcpy (&valueInt, value, INT_SIZE);
+	float convertedInt = (float)valueInt;
+
+	return RecordBasedFileManager::checkScanCondition((float) dataInt, compOp, &convertedInt);
+}
+
+bool Filter::checkCondition(float dataFloat, CompOp compOp, const void * value)
+{
+	float valueFloat;
+	memcpy (&valueFloat, value, REAL_SIZE);
+
+	switch (compOp)
+	{
+		case EQ_OP:  // =
+			return dataFloat == valueFloat;
+		break;
+		case LT_OP:  // <
+			return dataFloat < valueFloat;
+		break;
+		case GT_OP:  // >
+			return dataFloat > valueFloat;
+		break;
+		case LE_OP:  // <=
+			return dataFloat <= valueFloat;
+		break;
+		case GE_OP:  // >=
+			return dataFloat >= valueFloat;
+		break;
+		case NE_OP:  // !=
+			return dataFloat != valueFloat;
+		break;
+		case NO_OP:  // no condition
+			return true;
+		break;
+	}
+
+	// We should never get here.
+	return false;
+}
+
+bool Filter::checkCondition(char * dataString, CompOp compOp, const void * value)
+{
+	switch (compOp)
+	{
+		case EQ_OP:  // =
+			return strcmp(dataString, (char*) value) == 0;
+		break;
+		case LT_OP:  // <
+			return strcmp(dataString, (char*) value) < 0;
+		break;
+		case GT_OP:  // >
+			return strcmp(dataString, (char*) value) > 0;
+		break;
+		case LE_OP:  // <=
+			return strcmp(dataString, (char*) value) <= 0;
+		break;
+		case GE_OP:  // >=
+			return strcmp(dataString, (char*) value) >= 0;
+		break;
+		case NE_OP:  // !=
+			return strcmp(dataString, (char*) value) != 0;
+		break;
+		case NO_OP:  // no condition
+			return true;
+		break;
+	}
+
+	// We should never get here.
+	return false;
+}
+
 
 Project::Project(Iterator* input, const vector<string> &attrNames){
 	this->input = input;
@@ -75,7 +196,7 @@ RC Project::getNextTuple(void* data){
 			//currAttributeSize = 0;
 
 			//get size of attribute value and increase offset
-			if(inputAttributes[j].type == TypeInt || TypeReal){
+			if(inputAttributes[j].type == TypeInt || inputAttributes[j].type==TypeReal){
 				currAttributeSize = INT_SIZE;
 			}else{
 				memcpy(&currAttributeSize, (char*)buffer + bufferOffset, VARCHAR_LENGTH_SIZE);
